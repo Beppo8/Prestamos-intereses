8602b4259d892c9067a9fc68c3bf63f341194783
Beppo8 <beppo2488sm@gmail.com>
2025-02-28 12:37:10 -0600

    Initial setup Spring boot project

---
+++ src/main/java/com/example/intereses/InteresesApplication.java
@@ -0,0 +1,13 @@
+package com.example.intereses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteresesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InteresesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/intereses/InteresesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.intereses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteresesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
